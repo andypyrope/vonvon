5976751ca2a96482eda1799fab8b8709fa104bdb
Andrey Lalev <andypyrope@gmail.com>
2017-08-02 23:23:04 +0300

    Create project shell
    Functionality:
     - Create core/shell of project
    
    Tests:
     - Create empty 'test' task
    
    [Issue](https://github.com/andypyrope/vonvon/issues/1)
    [Project card](https://github.com/andypyrope/vonvon/projects/1#card-4052222)

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+
+gulp.task('test', function () {
+    console.log('No test task yet.');
+});
+
+gulp.task('default', function () {
+    console.log('No default task yet.');
+});
